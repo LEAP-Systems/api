2afb2ab47c3f9b42c22b507e63075ec90598b5c0
cSDes1gn <christian911@sympatico.ca>
2021-07-29 00:49:30 -0400

    fix api wrapper and integrate v1 bp

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,32 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-from flask_restful import Api
+from flask_restx import Api
+from flask.blueprints import Blueprint
 from flask_mongoengine import MongoEngine
-from app.v1.routes.users import Users
 
 
 # Globally accessible libraries
 db = MongoEngine()
-api = Api()
 
 
 def init_app():
     """Initialize the core application"""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
+    bp = Blueprint("api", __name__, url_prefix="/v1")
+    api = Api(
+        bp,
+        version="1.0",
+        title="LEAP Systems API",
+        description="Image Processing Backend",
+        contact_email="christian@leapsystems.online"
+    )
     # Initialize Plugins
-    api.init_app(app)
     db.init_app(app)
     with app.app_context():
         # Include our Routes
-        from app.v1.routes import captures
+        from app.v1.routes.captures import api as captures
+        app.register_blueprint(bp)
         # add resource endpoints
-        api.add_resource(Users, '/users/<int:id>', '/users', endpoint='user')
+        api.add_namespace(captures)
         return app
