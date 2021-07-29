136be4747825cdd171aacc9eef567e031604212a
cSDes1gn <christian911@sympatico.ca>
2021-07-29 01:03:07 -0400

    uncomment post method

--- app/v1/routes/captures.py
+++ app/v1/routes/captures.py
@@ -17,15 +17,17 @@ class Captures(Resource):
 
     @api.expect(image_schema, validate=True)
     def post(self):
+        payload: dict = request.get_json()
         # process payload args
-        app.logger.info("Received payload: %s", request.get_json())
+        app.logger.info("Received payload: %s", payload)
+        img = payload.get("data")
         # decode raw image data
-        # ib = base64.b64decode(img)
-        # # save png to disk (uuid for file collision avoidance)
-        # img_path = "{}/{}.png".format(app.config.get("STORAGE_PATH"), uuid.uuid4().hex)
-        # with open(img_path, 'wb') as open_file:
-        #     open_file.write(ib)
-        # app.logger.info("Successfully wrote capture to file system as %s", img_path)
+        ib = base64.b64decode(img)
+        # save png to disk (uuid for file collision avoidance)
+        img_path = "{}/{}.png".format(app.config.get("STORAGE_PATH"), uuid.uuid4().hex)
+        with open(img_path, 'wb') as open_file:
+            open_file.write(ib)
+        app.logger.info("Successfully wrote capture to file system as %s", img_path)
         # write capture resource to mongo
 
         # kickstart async processing request
