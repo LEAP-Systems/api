6a1dfd95e3554765b3d4ded8977355f2f3167a99
cSDes1gn <christian911@sympatico.ca>
2021-07-30 02:00:04 -0400

    docker compose config script

---
+++ scripts/config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source .env
+
+# handle all non-zero exit status codes with a slack notification
+trap 'handler $? $LINENO' ERR
+
+handler () {
+    printf "%b" "${FAIL} ✗ ${NC} ${0##*/} failed on line $2 with exit status $1\n"
+    exit $1
+}
+
+printf "%b" "${OKB}Checking for .env file in $DOCKER_ROOT/${NC}\n"
+if [ ! -f ./"$DOCKER_ROOT"/.env ]; then
+    cp .env ./"$DOCKER_ROOT"/.
+    printf "%b" "${OKG} ✓ ${NC} copied .env file to "$DOCKER_ROOT"/\n"
+else
+    printf "%b" "${OKG} ✓ ${NC} ok\n"
+fi
+printf "%b" "${OKB}Validating docker compose configuration${NC}\n"
+cd "$DOCKER_ROOT" || exit 1
+docker compose config
+cd - > /dev/null || exit 1
+printf "%b" "${OKG} ✓ ${NC}validation complete\n"
