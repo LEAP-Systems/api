2fa8e456f782bd4e12a9cbab669a118e6f7654ca
cSDes1gn <christian911@sympatico.ca>
2021-08-02 11:32:28 -0400

    uncaught exception app handler

--- wsgi.py
+++ wsgi.py
@@ -9,6 +9,16 @@ app = init_app()
 def resource_not_found(err):
     return jsonify(error=str(err)), 404
 
+@app.errorhandler(Exception)
+def internal_error(err):
+    response = {
+        'success': False,
+        'error': {
+            'type': 'UncaughtException',
+            'message': err
+        }
+    }
+    return jsonify(response), 500
 
 @app.route("/")
 def redirect_to_latest_api_version():
