c1d05cc6a52e9ea6862e86d963ac4c0e473cdcf5
cSDes1gn <christian911@sympatico.ca>
2021-08-02 16:16:31 -0400

    shellcheck

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,13 +7,12 @@ trap 'handler $? $LINENO' ERR
 
 handler () {
     printf "%b" "${FAIL} ✗ ${NC} ${0##*/} failed on line $2 with exit status $1\n"
-    exit $1
+    exit "$1"
 }
-DIFF=
 printf "%b" "${OKB}Checking for .env diff in $DOCKER_ROOT/${NC}\n"
-if [ ! -f ./"$DOCKER_ROOT"/.env ] || [ $(diff ".env" "./$DOCKER_ROOT/.env") ] ; then
+if [ ! -f ./"$DOCKER_ROOT"/.env ] || [ "$(diff ".env" "./$DOCKER_ROOT/.env")" ] ; then
     cp .env ./"$DOCKER_ROOT"/.
-    printf "%b" "${OKG} ✓ ${NC} copied .env file to "$DOCKER_ROOT"/\n"
+    printf "%b" "${OKG} ✓ ${NC} copied .env file to $DOCKER_ROOT/\n"
 else
     printf "%b" "${OKG} ✓ ${NC} ok\n"
 fi
