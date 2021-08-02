030089cad1813a086b0b9510898f10f6de58ed0e
cSDes1gn <christian911@sympatico.ca>
2021-08-02 16:20:30 -0400

    use admin default

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config:
 
     # db conf
     MONGODB_SETTINGS = {
+        'db': 'admin',
         'port': int(os.environ['MONGO_PORT']),
         'host': os.environ['MONGO_HOST'],
         'username': os.environ['MONGO_USER'],
