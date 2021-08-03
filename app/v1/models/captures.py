dc1e40cbf12943d0dd94970f468bb792550526cc
cSDes1gn <christian911@sympatico.ca>
2021-08-03 00:23:58 -0400

    add api model for marshalling, change datetimes to iso ts

--- app/v1/models/captures.py
+++ app/v1/models/captures.py
@@ -2,6 +2,7 @@ from enum import unique
 import mongoengine as me
 from datetime import datetime
 from typing import Any, Dict
+from flask_restx import fields
 from flask_restx.reqparse import RequestParser
 from werkzeug.datastructures import FileStorage
 
@@ -15,17 +16,26 @@ class CaptureModel:
 class Capture(me.Document):
 
     path = me.StringField(unique=True)
-    created_at = me.DateTimeField(required=True)
-    updated_at = me.DateTimeField(required=True, default=datetime.now())
+    created_at = me.StringField(required=True)
+    updated_at = me.StringField(required=True, default=datetime.now().isoformat())
 
     def __repr__(self):
         return "{}: {}".format(self.__class__.__name__, vars(self))
 
     def save(self, *args, **kwargs):
         if not self.created_at:
-            self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+            self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
         return super().save(*args, **kwargs)
+
+    @staticmethod
+    def api_model() -> Dict[str, Any]:
+        return {
+            'id': fields.String,
+            'path': fields.String,
+            'created_at': fields.String,
+            'updated_at': fields.String
+        }
 
     @staticmethod
     def post_model(model: RequestParser) -> RequestParser:
