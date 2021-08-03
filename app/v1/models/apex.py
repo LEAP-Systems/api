8527b5ee776431bfbf330d7d03202a96fcf2cbd3
cSDes1gn <christian911@sympatico.ca>
2021-08-03 00:35:53 -0400

    linting

--- app/v1/models/apex.py
+++ app/v1/models/apex.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import mongoengine as me
-from flask_restx import fields
-from typing import Any, Dict
 from app.v1.models.gauss import GaussCurve
+
 
 class Apex(me.EmbeddedDocument):
     initial = me.EmbeddedDocumentField(GaussCurve)
