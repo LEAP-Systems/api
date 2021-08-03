bf2ee9cb58ded003a3946b7d86e15f9aca236974
cSDes1gn <christian911@sympatico.ca>
2021-08-03 18:16:00 -0400

    add model

---
+++ app/v1/models/model.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from typing import Any, Dict
+from flask_restx.reqparse import RequestParser
+from flask_restx import fields
+import mongoengine as me
+from app.v1.models.capture import Capture
+from app.v1.models.apex import Apex
+from app.v1.models.processing import Dialation, Erosion, GaussianBlur, Threshold
+
+
+class Model(me.Document):
+    capture_id = me.ReferenceField(Capture, required=True)
+    created_at = me.StringField(required=True)
+    gaussian_blur = me.EmbeddedDocumentField(GaussianBlur)
+    erosions = me.EmbeddedDocumentField(Erosion)
+    dialations = me.EmbeddedDocumentField(Dialation)
+    threshold = me.EmbeddedDocumentField(Threshold)
+    elapsed = me.FloatField(required=True)
+    chi_squared_error = me.FloatField(required=True)
+    apexes = me.EmbeddedDocumentListField(Apex)
+
+    @staticmethod
+    def api_model() -> Dict[str, Any]:
+        return {
+            'id': fields.String(required=True, description="model id"),
+            'capture_id': fields.String(required=True, description="capture id"),
+            'created_at': fields.String(required=True, description="model creation timestamp"),
+            'chi_squared_error': fields.Float(required=True, description="chi squared error"),
+            'elapsed': fields.Float(required=True, description="curve fitting elapsed time"),
+            # 'apexes': fields.List(Apex, description="optimized apexes"),
+            # 'gaussian_blur': fields.Nested(GaussianBlur)
+        }
+
+    @staticmethod
+    def post_model(model: RequestParser) -> RequestParser:
+        model.add_argument(
+            "capture_id",
+            type=str,
+            location='body',
+            help="Capture id for processing"
+        )
+        model.add_argument(
+            "iterations",
+            type=int,
+            location='body',
+            help="Minimization iterations"
+        )
+        model.add_argument(
+            "divisor",
+            type=int,
+            location='body',
+            help="Resolution divisor"
+        )
+        return model
