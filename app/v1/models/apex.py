d05c742e076597f318a73d39040200aba303678d
cSDes1gn <christian911@sympatico.ca>
2021-08-04 16:08:29 -0400

    seperate gaussian and apex models

--- app/v1/models/apex.py
+++ app/v1/models/apex.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
+from typing import Any, Dict
+from flask_restx import fields
 import mongoengine as me
-
-
-class GaussianCurve(me.EmbeddedDocument):
-    amplitude = me.FloatField()
-    sigma_x = me.FloatField()
-    sigma_y = me.FloatField()
-    mu_x = me.IntField()
-    mu_y = me.IntField()
+from app.v1.namespaces.gaussian import gaussian_model
+from app.v1.models.gaussian import GaussianCurve
 
 
 class Apex(me.EmbeddedDocument):
     initial = me.EmbeddedDocumentField(GaussianCurve)
     optimized = me.EmbeddedDocumentField(GaussianCurve)
+
+    @staticmethod
+    def api_model() -> Dict[str, Any]:
+        return {
+            'initial': fields.Nested(
+                gaussian_model,
+                description="Initial gaussian parameters",
+                required=True
+            ),
+            'optimized': fields.Nested(
+                gaussian_model,
+                description="Initial gaussian parameters",
+                required=True
+            ),
+        }
