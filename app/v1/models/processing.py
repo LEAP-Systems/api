0107e5e88c18f833d6476db8ee7833e26076920d
cSDes1gn <christian911@sympatico.ca>
2021-08-06 11:48:30 -0400

    update processing model

--- app/v1/models/processing.py
+++ app/v1/models/processing.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import mongoengine as me
+from datetime import datetime
+from typing import Any, Dict
+from flask_restx import fields
 
 
 class GaussianBlur(me.EmbeddedDocument):
@@ -7,11 +10,31 @@ class GaussianBlur(me.EmbeddedDocument):
     kernel_height = me.IntField(required=True)
 
 
-class Erosion(me.EmbeddedDocument):
-    kernel_width = me.IntField(required=True)
-    kernel_height = me.IntField(required=True)
-    iterations = me.IntField(required=True)
+class Erosion(me.Document):
 
+    iterations = me.IntField(required=True, min_value=1)
+    # kernel shape must be odd numbers
+    kernel_width = me.IntField(required=True, min_value=1)
+    kernel_height = me.IntField(required=True, min_value=1)
+    created_at = me.StringField(required=True)
+
+    def __repr__(self):
+        return "{}: {}".format(self.__class__.__name__, vars(self))
+
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = datetime.now().isoformat()
+        return super().save(*args, **kwargs)
+
+    @staticmethod
+    def api_model() -> Dict[str, Any]:
+        return {
+            'id': fields.String(required=True, description="capture id"),
+            'iterations': fields.Integer(required=True, min=1, description="erosion iterations"),
+            'kernel_width': fields.Integer(required=True, min=1, description="kernel width"),
+            'kernel_height': fields.Integer(required=True, min=1, description="kernel height"),
+            'created_at': fields.String(required=True, description="Time of upload"),
+        }
 
 class Dialation(me.EmbeddedDocument):
     kernel_width = me.IntField(required=True)
