a423463566e548d1874c0cb9f437a548a62d7c95
cSDes1gn <christian911@sympatico.ca>
2021-08-06 23:46:55 -0400

    thresholding model

---
+++ app/v1/models/threshold.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import mongoengine as me
+from datetime import datetime
+from typing import Any, Dict
+from flask_restx import fields
+from enum import Enum
+
+class ThresholdTypes(Enum):
+    INVERSE = 'inverse'
+    NORMAL = 'normal'
+
+class Threshold(me.Document):
+
+    threshold = me.IntField(required=True, min_value=0, max_value=255)
+    output = me.IntField(required=True, min_value=0, max_value=255)
+    type = me.EnumField(ThresholdTypes, default=ThresholdTypes.NORMAL, required=True)
+    created_at = me.StringField(required=True)
+
+    def __repr__(self):
+        return "{}: {}".format(self.__class__.__name__, vars(self))
+
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = datetime.now().isoformat()
+        return super().save(*args, **kwargs)
+
+    @staticmethod
+    def api_model() -> Dict[str, Any]:
+        return {
+            'id': fields.String(required=True, description="threshold id"),
+            'type': fields.String(required=True, description="thresholding type (inverse, normal)"),
+            'output': fields.Integer(required=True, min=0, max=255, description="erosion iterations"),
+            'threshold': fields.Integer(required=True, min=0, max=255, description="threshold value"),
+            'created_at': fields.String(required=True, description="created ISO datetime"),
+        }
