ac38714ccc8a8711b69da2ad91546b476d4b2eb7
cSDes1gn <christian911@sympatico.ca>
2021-08-06 23:44:58 -0400

    apply namespaces to nested fields

--- app/v1/models/model.py
+++ app/v1/models/model.py
@@ -6,17 +6,22 @@ from flask_restx.reqparse import RequestParser
 from flask_restx import fields
 from app.v1.models.capture import Capture
 from app.v1.namespaces.apex import apex_model
+from app.v1.namespaces.dialation import dialation_model
+from app.v1.namespaces.threshold import threshold_model
+from app.v1.namespaces.gaussian_blur import gaussian_blur_model
 from app.v1.namespaces.erosion import erosion_model
-from app.v1.models.processing import Dialation, Erosion, GaussianBlur, Threshold
-
+from app.v1.models.dialation import Dialation
+from app.v1.models.threshold import Threshold
+from app.v1.models.erosion import Erosion
+from app.v1.models.gaussian_blur import GaussianBlur
 
 class Model(me.Document):
     capture_id = me.ReferenceField(Capture, required=True)
     created_at = me.StringField(required=True)
-    gaussian_blur = me.EmbeddedDocumentField(GaussianBlur)
+    gaussian_blur = me.ReferenceField(GaussianBlur)
     erosion = me.ReferenceField(Erosion)
-    dialation = me.EmbeddedDocumentField(Dialation)
-    threshold = me.EmbeddedDocumentField(Threshold)
+    dialation = me.ReferenceField(Dialation)
+    threshold = me.ReferenceField(Threshold)
     elapsed = me.FloatField(required=True)
     apexes = me.EmbeddedDocumentListField(Apex)
 
@@ -30,18 +35,18 @@ class Model(me.Document):
                 erosion_model,
                 description="Erosion parameters"
             ),
-            # 'dialation': fields.Nested(
-            #     dialation_model,
-            #     description="Dialation parameters"
-            # ),
-            # 'threshold': fields.Nested(
-            #     threshold_model,
-            #     description="Thresholding parameters"
-            # ),
-            # 'gaussian_blur': fields.Nested(
-            #     gaussian_blur_model,
-            #     description="Gaussian blur parameters"
-            # ),
+            'dialation': fields.Nested(
+                dialation_model,
+                description="Dialation parameters"
+            ),
+            'threshold': fields.Nested(
+                threshold_model,
+                description="Thresholding parameters"
+            ),
+            'gaussian_blur': fields.Nested(
+                gaussian_blur_model,
+                description="Gaussian blur parameters"
+            ),
             'elapsed': fields.Float(required=True, description="curve fitting elapsed time"),
             'apexes': fields.List(
                 fields.Nested(
