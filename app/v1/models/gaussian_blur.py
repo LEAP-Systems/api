1ba080d79349cf57a014c5ff40d71bbcf21c315f
cSDes1gn <christian911@sympatico.ca>
2021-08-06 23:44:29 -0400

    gb model

---
+++ app/v1/models/gaussian_blur.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import mongoengine as me
+from datetime import datetime
+from typing import Any, Dict
+from flask_restx import fields
+
+
+class GaussianBlur(me.Document):
+
+    # kernel shape must be odd numbers
+    kernel_width = me.IntField(required=True, min_value=1)
+    kernel_height = me.IntField(required=True, min_value=1)
+    created_at = me.StringField(required=True)
+
+    def __repr__(self):
+        return "{}: {}".format(self.__class__.__name__, vars(self))
+    
+    def save(self, *args, **kwargs):
+        if not self.created_at:
+            self.created_at = datetime.now().isoformat()
+        return super().save(*args, **kwargs)
+
+    @staticmethod
+    def api_model() -> Dict[str, Any]:
+        return {
+            'id': fields.String(required=True, description="Gaussian blur id"),
+            'kernel_width': fields.Integer(required=True, min=1, description="kernel width"),
+            'kernel_height': fields.Integer(required=True, min=1, description="kernel height"),
+            'created_at': fields.String(required=True, description="created ISO datetime"),
+        }
