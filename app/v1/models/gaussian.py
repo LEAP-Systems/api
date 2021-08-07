68eeea47a221e247cc013fdba4b104baeef574ae
cSDes1gn <christian911@sympatico.ca>
2021-08-06 23:44:41 -0400

    update curve model

--- app/v1/models/gaussian.py
+++ app/v1/models/gaussian.py
@@ -4,18 +4,19 @@ from flask_restx import fields
 
 
 class GaussianCurve(me.EmbeddedDocument):
-    amplitude = me.FloatField()
-    sigma_x = me.FloatField()
-    sigma_y = me.FloatField()
-    mu_x = me.IntField()
-    mu_y = me.IntField()
+
+    amplitude = me.FloatField(required=True, min_value=0, max_value=255)
+    sigma_x = me.FloatField(required=True, min_value=0)
+    sigma_y = me.FloatField(required=True, min_value=0)
+    mu_x = me.IntField(required=True, min_value=0)
+    mu_y = me.IntField(required=True, min_value=0)
 
     @staticmethod
     def api_model() -> Dict[str, Any]:
         return {
-            'amplitude': fields.Float(required=True, description="peak amplitude"),
-            'sigma_x': fields.Float(required=True, description="standard deviation (σ) in x"),
-            'sigma_y': fields.Float(required=True, description="standard deviation (σ) in y"),
-            'mu_x': fields.Integer(required=True, description="mean (μ) in x"),
-            'mu_y': fields.Integer(required=True, description="mean (μ) in y"),
+            'amplitude': fields.Float(required=True, min=0, max=255, description="peak amplitude"),
+            'sigma_x': fields.Float(required=True, min=0, description="standard deviation (σ) in x"),
+            'sigma_y': fields.Float(required=True, min=0, description="standard deviation (σ) in y"),
+            'mu_x': fields.Integer(required=True, min=0, description="mean (μ) in x"),
+            'mu_y': fields.Integer(required=True, min=0, description="mean (μ) in y"),
         }
