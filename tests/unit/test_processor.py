f8366244caf292c4a7ef0b5bb8d2ec2717b834e1
cSDes1gn <christian911@sympatico.ca>
2021-08-31 01:36:57 -0400

    configure unittest env with pytest

---
+++ tests/unit/test_processor.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Processor Model Unittests
+=========================
+Modified: 2021-08
+
+Copyright © 2021 LEAP. All Rights Reserved.
+"""
+
+import pytest
+from app.v1.models.processor import Processor
+from tests import resources
+
+
+@pytest.fixture(scope='function')
+def new_processor():
+    processor = Processor(**resources.PROCESSOR_PAYLOAD)
+    return processor
+
+
+def test_processor(new_processor: Processor):
+    assert new_processor.dialation is not None
+    assert new_processor.erosion is not None
+    assert new_processor.gaussian_blur is not None
+    assert new_processor.threshold is not None
